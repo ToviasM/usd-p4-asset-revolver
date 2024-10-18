66b859ed5d5a084c33b198285f40f083a57a06c5
ToviasM <88663584+ToviasM@users.noreply.github.com>
2024-10-18 16:19:27 -0400

    removed setup.bat

--- setup.bat
+++
@@ -1,8 +0,0 @@
-
-set AR_DCC_NAME=houdini
-set HFS=D:/SideFX/Houdini20.5.332 # For example "C:\Program Files\Side Effects Software\<HoudiniVersion>"
-set AR_RESOLVER_NAME=perforceResolver
-set PYTHONPATH=%PYTHONPATH%
-set PATH=%PATH%
-
-set TF_DEBUG=AR_RESOLVER_INIT
